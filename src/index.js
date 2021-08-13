887a0b3a43129f7741a81ea150811aa580d79218
Andre Maesha <andremaesha@gmail.com>
2021-08-14 00:29:51 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+cloudinary.config({
+    cloud_name: process.env.API_CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+});
+
+cloudinary.v2.uploader.upload(
+    path.join(__dirname, "../images/login-bg.jpg"),
+    {
+        use_filename: true,
+        unique_filename: false,
+    },
+    (error, result) => {
+        if (!error) {
+            console.log(result);
+        }
+        console.log(error);
+    }
+);
